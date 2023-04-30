db814e3edbacd2b726953b73d08b26292a8779ac
Andy CHENG <ziqiang.cheng@qq.com>
2023-04-30 22:34:44 +0800

    init repository

---
+++ src/chassis/include/chassis/chassis_node.hpp
@@ -0,0 +1,12 @@
+#ifndef __CHASSIS_NODE_HPP_
+#define __CHASSIS_NODE_HPP_
+
+#include "ros/ros.h"
+
+namespace chassis {
+    class chassis {
+        
+    };
+};
+
+#endif
---
+++ src/chassis/src/chassis_node.cpp
@@ -0,0 +1,13 @@
+#include "chassis/chassis_node.hpp"
+
+int main(int argc, char **argv)
+{
+    ros::init(argc, argv, "chassis");
+    ros::NodeHandle nh;
+
+    while(ros::ok)
+    {
+
+    }
+    return 0;
+}
